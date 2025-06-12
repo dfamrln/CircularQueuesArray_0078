e80b8aa53be1f1e475ad5837029895ea76a4bcf6
dfamrln <mhdffa1@gmail.com>
2025-06-12 17:44:51 +0700

    Update

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -36,5 +36,13 @@ class Queues
                 REAR = 0;
             }
             else
+            {
+                // JIka REAR berada di posisi terakhir array, kembali ke awal array
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
         }
 }
